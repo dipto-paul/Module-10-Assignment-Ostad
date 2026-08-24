9c328972a12bb0cf237dbe105c67380a80c752ed
Dipto Paul <diptopaul.contact@gmail.com>
2026-08-24 20:25:20 +0600

    added urls and geted flutter pub

---
+++ lib/utils/urls.dart
@@ -0,0 +1,8 @@
+
+class Urls {
+  static String baseURL = 'https://crud-api-ostad-live.onrender.com/api/v1';
+  static String readURL = '$baseURL/ReadProduct';
+  static String createURL = '$baseURL/CreateProduct';
+  static String updateURL = '$baseURL/UpdateProduct/65abe919f68794d36c5b7ffa';
+  static String deleteURL = '$baseURL/DeleteProduct/65abe8b31e27100ba914f2e5';
+}
