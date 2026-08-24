d1ca92d3037a189ef971a55afbae6142458b96de
Dipto Paul <diptopaul.contact@gmail.com>
2026-08-25 01:06:07 +0600

    UI ready

--- lib/screens/add_product.dart
+++ lib/screens/add_product.dart
@@ -12,7 +12,7 @@ class _AddProductState extends State<AddProduct> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Color(0xFF4382DF),
+        backgroundColor: Color(0xFF5346E0),
         title: Text('Add Products',style: TextStyle(
           fontWeight: FontWeight.bold,
           color: Colors.white,
@@ -27,6 +27,76 @@ class _AddProductState extends State<AddProduct> {
           },
           icon: Icon(Icons.arrow_back, size: 30,color: Colors.white,),
         ),
+      ),
+
+      body: SafeArea(
+        child: SingleChildScrollView(
+          padding: EdgeInsets.all(20),
+          child: Column(
+            children: [
+              SizedBox(height: 30),
+              TextFormField(
+
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    prefixIcon: Icon(Icons.person),
+                    hintText: "Product Name",
+                    hintStyle: TextStyle(color: Colors.black, )
+
+                ),
+              ),
+
+              SizedBox(height: 30),
+
+              TextFormField(
+
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    prefixIcon: Icon(Icons.price_check_outlined),
+                    hintText: "Price",
+                    hintStyle: TextStyle(color: Colors.black, )
+
+                ),
+              ),
+
+              SizedBox(height: 30),
+
+              TextFormField(
+
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    prefixIcon: Icon(Icons.image),
+                    hintText: "Image",
+                    hintStyle: TextStyle(color: Colors.black, )
+
+                ),
+              ),
+
+              SizedBox(height: 70),
+
+              ElevatedButton(onPressed: (){},
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Color(0xFF5346E0),
+                  //padding: EdgeInsets.vertical(15),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(14),
+                  ),
+                  elevation: 0,
+                ),
+                child: const Text(
+                  'Save Product',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 16,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+
+              ),
+            ],
+          ),
+        ),
+
       ),
 
     );
--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -10,92 +10,114 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-
-
-  productDialog(){
-    showDialog(context: context, builder: (context) => AlertDialog(
-      title: Text("Add Product"),
-      content: Column(
-        children: [
-
-        ],
-      ),
-
-    ));
-  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Color(0xFF4382DF),
-        title: Text('Products',style: TextStyle(
-          fontWeight: FontWeight.bold,
-          color: Colors.white,
-          fontSize: 25,
-        ),),
+        backgroundColor: Color(0xFF5346E0),
+        title: Text(
+          'Products',
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            color: Colors.white,
+            fontSize: 25,
+          ),
+        ),
         centerTitle: true,
         elevation: 0,
         actions: [
           IconButton(
             iconSize: 36,
             color: Colors.white,
-            icon: Icon(Icons.search), onPressed: (){
-          },
-          )
+            icon: Icon(Icons.search),
+            onPressed: () {},
+          ),
         ],
         leading: IconButton(
-          onPressed: (){},
-          icon: Icon(Icons.menu, size: 30,color: Colors.white,),
+          onPressed: () {},
+          icon: Icon(Icons.menu, size: 30, color: Colors.white),
         ),
       ),
 
       body: GridView.builder(
-          padding: EdgeInsets.all(8),
-              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 2,
-                crossAxisSpacing: 12,
-                mainAxisSpacing: 12,
-                childAspectRatio: 0.80,
+        padding: EdgeInsets.all(8),
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          crossAxisSpacing: 12,
+          mainAxisSpacing: 12,
+          childAspectRatio: 0.60,
+        ),
+
+        itemCount: 10,
+        itemBuilder: (context, index) {
+          return Column(
+            children: [
+              SizedBox(
+                child: Image.network(
+                  'https://media.istockphoto.com/id/1412240771/photo/headphones-on-white-background.jpg?s=612x612&w=0&k=20&c=DwpnlOcMzclX8zJDKOMSqcXdc1E7gyGYgfX5Xr753aQ=',
+                ),
               ),
 
-              itemCount: 10,
-              itemBuilder: (context, index){
+              Text(
+                "Wireless Headphone",
+                style: TextStyle(fontSize: 17, fontWeight: FontWeight.bold),
+              ),
 
-                return Column(
-                  children: [
-                    SizedBox(
-                      child: Image.network('https://media.istockphoto.com/id/1412240771/photo/headphones-on-white-background.jpg?s=612x612&w=0&k=20&c=DwpnlOcMzclX8zJDKOMSqcXdc1E7gyGYgfX5Xr753aQ=')),
+              Text(
+                "Price: 300",
+                style: TextStyle(
+                  fontSize: 17,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.indigoAccent,
+                ),
+              ),
+              SizedBox(height: 5),
 
-                    Text("Wireless Headphone", style: TextStyle(
-                      fontSize: 17,
-                      fontWeight: FontWeight.bold,
-                    ),),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  IconButton(
+                    onPressed: () {},
+                    icon: Icon(Icons.edit),
+                    style: IconButton.styleFrom(
+                      backgroundColor: Colors.white,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(7),
+                      ),
+                      elevation: 0,
+                    ),
+                  ),
 
-                    Text("Price: 300", style: TextStyle(
-                      fontSize: 17,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.indigoAccent,
-                    ),),
-                    SizedBox(height: 5,)
-                  ],
-                );
-          }),
+                  IconButton(
+                    onPressed: () {},
+                    icon: Icon(Icons.delete, color: Colors.red),
+                    style: IconButton.styleFrom(
+                      backgroundColor: Colors.white,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(7),
+                      ),
+                      elevation: 0,
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          );
+        },
+      ),
 
       floatingActionButton: FloatingActionButton(
-        backgroundColor: Colors.indigoAccent,
+        backgroundColor: Color(0xFF5346E0),
         foregroundColor: Colors.white,
 
         onPressed: () {
           Navigator.push(
             context,
-            MaterialPageRoute(
-              builder: (context) => AddProduct(),
-            ),
+            MaterialPageRoute(builder: (context) => AddProduct()),
           );
         },
-        child: Icon(Icons.add,),
+        child: Icon(Icons.add),
       ),
-
     );
   }
 }
