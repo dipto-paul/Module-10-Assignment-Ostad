e199c6ea2577a7e0cc459b2493d5b201b5611fa1
Dipto Paul <diptopaul.contact@gmail.com>
2026-08-24 21:54:29 +0600

    appbar completed

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -10,6 +11,30 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Color(0xFF4382DF),
+        title: Text('Products',style: TextStyle(
+          fontWeight: FontWeight.bold,
+          color: Colors.white,
+          fontSize: 25,
+        ),),
+        centerTitle: true,
+        elevation: 0,
+        actions: [
+          IconButton(
+            iconSize: 36,
+            color: Colors.white,
+            icon: Icon(Icons.search), onPressed: (){
+          },
+          )
+        ],
+        leading: IconButton(
+          onPressed: (){},
+          icon: Icon(Icons.menu, size: 30,color: Colors.white,),
+        ),
+      ),
+
+    );
   }
 }
