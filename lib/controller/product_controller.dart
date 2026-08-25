f8c127a5a622cba66f0ddbddce54b39ea609cd93
Dipto Paul <diptopaul.contact@gmail.com>
2026-08-25 10:52:09 +0600

    aded product_controller

---
+++ lib/controller/product_controller.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/services.dart';
+import 'package:module10_assignment_ostad/utils/urls.dart';
+import 'package:http/http.dart' as http;
+
+import '../model/product_model.dart';
+class ProductController {
+
+  List<Data>products = [];
+
+  Future getProduct() async{
+    final url = Uri.parse(Urls.readURL);
+
+    final response = await http.get(url);
+
+    print(response.statusCode);
+    print(response.body);
+
+    if(response.statusCode == 200){
+
+    }
+  }
+}
